62e82105838d1cfb6dec2b87d0dcdc82becbf081
Tony Atkins <tony@raisingthefloor.org>
2016-07-20 16:25:46 +0200

    Wired all existing tests into `all-tests.js`.

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -1,6 +1,9 @@
 /* eslint-env node */
 "use strict";
 
+require("./js/actions");
+require("./js/browser-navigation");
+require("./js/executeScript");
 require("./js/findElement");
 require("./js/findElements");
 require("./js/get");
--- tests/js/get/index.js
+++ tests/js/get/index.js
@@ -1,7 +1,7 @@
 /* eslint-env node */
 "use strict";
 
-// TODO:  Review with Antranig, running multiple tests appears to cause problems.
+// TODO:  Review with Antranig, running multiple tests appears to cause problems on OS X, but not Windows.
 require("./local");
 require("./remote");
 
