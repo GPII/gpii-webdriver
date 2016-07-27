c52220da955cc6b801a004bdd4c2fee24c5aff04
Tony Atkins <tony@raisingthefloor.org>
2016-07-27 12:14:05 +0200

    Added additional text input tests.

---
+++ tests/js/actions/browser-actions-text-input.js
@@ -0,0 +1,109 @@
+/*
+
+    Test the "findElement" function with a single element.
+
+ */
+/* eslint-env node */
+"use strict";
+var fluid = require("infusion");
+var gpii = fluid.registerNamespace("gpii");
+
+require("../../../");
+gpii.webdriver.loadTestingSupport();
+
+fluid.defaults("gpii.tests.webdriver.actions.text.caseHolder", {
+    gradeNames: ["gpii.test.webdriver.caseHolder"],
+    fileUrl: "%gpii-webdriver/tests/js/actions/html/text.html",
+    rawModules: [{
+        name: "Testing keyboard text input...",
+        tests: [
+            {
+                name: "Type in a single string...",
+                type: "test",
+                sequence: [
+                    {
+                        func: "{testEnvironment}.webdriver.get",
+                        args: ["@expand:gpii.test.webdriver.resolveFileUrl({that}.options.fileUrl)"]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onGetComplete",
+                        listener: "{testEnvironment}.webdriver.actionsHelper",
+                        args:     [{ sendKeys: ["This is really something"] }]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onActionsHelperComplete",
+                        listener: "{testEnvironment}.webdriver.findElement",
+                        args:     [{ id: "text-field"}]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onFindElementComplete",
+                        listener: "gpii.webdriver.test.testElementValue",
+                        args:     ["The text should be as entered...", "{arguments}.0", "This is really something"] // message, element, expectedValue, jqUnitFn
+                    }
+                ]
+            },
+            {
+                name: "Type in an array of strings...",
+                type: "test",
+                sequence: [
+                    {
+                        func: "{testEnvironment}.webdriver.get",
+                        args: ["@expand:gpii.test.webdriver.resolveFileUrl({that}.options.fileUrl)"]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onGetComplete",
+                        listener: "{testEnvironment}.webdriver.actionsHelper",
+                        args:     [{ sendKeys: ["This ", "is ", "really ", "something"]}]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onActionsHelperComplete",
+                        listener: "{testEnvironment}.webdriver.findElement",
+                        args:     [{ id: "text-field"}]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onFindElementComplete",
+                        listener: "gpii.webdriver.test.testElementValue",
+                        args:     ["The text should be as entered...", "{arguments}.0", "This is really something"] // message, element, expectedValue, jqUnitFn
+                    }
+                ]
+            },
+            {
+                name: "Type in unicode...",
+                type: "test",
+                sequence: [
+                    {
+                        func: "{testEnvironment}.webdriver.get",
+                        args: ["@expand:gpii.test.webdriver.resolveFileUrl({that}.options.fileUrl)"]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onGetComplete",
+                        listener: "{testEnvironment}.webdriver.actionsHelper",
+                        // "I can eat glass, it doesn't hurt me.", in Armenian. Taken from http://www.columbia.edu/~fdc/utf8/
+                        args:     [{ sendKeys: ["Կրնամ ապակի ուտել և ինծի անհանգիստ չըներ։"]}]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onActionsHelperComplete",
+                        listener: "{testEnvironment}.webdriver.findElement",
+                        args:     [{ id: "text-field"}]
+                    },
+                    {
+                        event:    "{testEnvironment}.webdriver.events.onFindElementComplete",
+                        listener: "gpii.webdriver.test.testElementValue",
+                        args:     ["The text should be as entered...", "{arguments}.0", "Կրնամ ապակի ուտել և ինծի անհանգիստ չըներ։"] // message, element, expectedValue, jqUnitFn
+                    }
+                ]
+            }
+        ]
+    }]
+});
+
+fluid.defaults("gpii.tests.webdriver.actions.text.environment", {
+    gradeNames: ["gpii.test.webdriver.testEnvironment"],
+    components: {
+        caseHolder: {
+            type: "gpii.tests.webdriver.actions.text.caseHolder"
+        }
+    }
+});
+
+gpii.test.webdriver.allBrowsers({ baseTestEnvironment: "gpii.tests.webdriver.actions.text.environment" });
--- tests/js/actions/index.js
+++ tests/js/actions/index.js
@@ -3,3 +3,4 @@
 
 require("./browser-actions-failure");
 require("./browser-actions-tab-navigation");
+require("./browser-actions-text-input");
